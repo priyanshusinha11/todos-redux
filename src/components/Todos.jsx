9d674d38a53abb2f9c8ef3ce269c852a90b10444
priyanshusinha11 <priyanshusinha.2807@gmail.com>
2024-05-22 22:50:47 +0530

    ui changes

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -3,59 +3,66 @@ import { useSelector, useDispatch } from 'react-redux';
 import { removeTodo, updateTodo } from '../features/todo/todoSlice';
 
 function TodoItem({ todo }) {
-    const [editMode, setEditMode] = useState(false);
-    const [updatedText, setUpdatedText] = useState(todo.text);
+    const [isTodoEditable, setIsTodoEditable] = useState(false);
+    const [todoMsg, setTodoMsg] = useState(todo.text);
     const dispatch = useDispatch();
-    const handleUpdateSubmit = () => {
-        dispatch(updateTodo({ id: todo.id, text: updatedText }));
-        setEditMode(false);
+
+    const editTodo = () => {
+        dispatch(updateTodo({ id: todo.id, text: todoMsg }));
+        setIsTodoEditable(false);
+    };
+
+    const toggleCompleted = () => {
+        dispatch(updateTodo({ id: todo.id, completed: !todo.completed }));
     };
 
     return (
-        <>
-            {editMode ? (
-                <form onSubmit={handleUpdateSubmit}>
-                    <input
-                        type="text"
-                        value={updatedText}
-                        onChange={(e) => setUpdatedText(e.target.value)}
-                        autoFocus
-                    />
-                    <button type="submit">Save</button>
-                </form>
-            ) : (
-                <div className='text-white'>
-                    {todo.text}
-                    <button
-                        onClick={() => setEditMode(true)}
-                        className="ml-4 text-white bg-blue-500 border-0 py-1 px-4 focus:outline-none hover:bg-blue-600 rounded text-md"
-                    >
-                        Edit
-                    </button>
-                </div>
-            )}
+        <div className={`flex border border-black/10 rounded-lg px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300  text-black ${todo.completed ? "bg-[#c6e9a7]" : "bg-[#ccbed7]"}`}>
+            <input
+                type="checkbox"
+                className="cursor-pointer"
+                checked={todo.completed}
+                onChange={toggleCompleted}
+            />
+            <input
+                type="text"
+                className={`border outline-none w-full bg-transparent rounded-lg ${isTodoEditable ? "border-black/10 px-2" : "border-transparent"} ${todo.completed ? "line-through" : ""}`}
+                value={todoMsg}
+                onChange={(e) => setTodoMsg(e.target.value)}
+                readOnly={!isTodoEditable}
+            />
             <button
-                onClick={() => dispatch(removeTodo(todo.id))}
-                className="text-white bg-red-500 border-0 py-1 px-4 focus:outline-none hover:bg-red-600 rounded text-md ml-4"
+                className="inline-flex w-8 h-8 rounded-lg text-sm border border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0 disabled:opacity-50"
+                onClick={() => {
+                    if (todo.completed) return;
+
+                    if (isTodoEditable) {
+                        editTodo();
+                    } else setIsTodoEditable((prev) => !prev);
+                }}
+                disabled={todo.completed}
             >
-                Delete
+                {isTodoEditable ? "📁" : "✏️"}
             </button>
-        </>
+            <button
+                className="inline-flex w-8 h-8 rounded-lg text-sm border border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0"
+                onClick={() => dispatch(removeTodo(todo.id))}
+            >
+                ❌
+            </button>
+        </div>
     );
 }
 
 function Todos() {
     const todos = useSelector(state => state.todos);
-    //const dispatch = useDispatch();
 
     return (
-        <>
-            <ul className="list-none">
-                {todos.map((todo) => (
-                    <TodoItem key={todo.id} todo={todo} />
-                ))}
-            </ul>
-        </>
+        <ul className="list-none">
+            {todos.map((todo) => (
+                <TodoItem key={todo.id} todo={todo} />
+            ))}
+        </ul>
     );
 }
 
